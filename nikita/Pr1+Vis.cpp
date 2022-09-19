087bcc5929d70adc071a8faf711acaea176ee622
NikkiZero <113890661+NikkiZero@users.noreply.github.com>
2022-09-19 11:43:25 +0300

    Add files via upload

---
+++ nikita/Pr1+Vis.cpp
@@ -0,0 +1,269 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <locale.h>
+#include <algorithm>
+
+using namespace std;
+
+class triangle
+    //создаем класс треугольник
+{
+private:
+    double a, b, c;
+    //стороны треугольника
+public:
+    double GetA()
+    {
+        cout << "a = " << a << endl;
+        return a;
+    }
+    double GetB()
+    {
+        cout << "b = " << b << endl;
+        return b;
+    }
+    double GetC()
+    {
+        cout << "c = " << c << endl;
+        return c;
+    }
+    void SetA(double valueA)
+    {
+        a = valueA;
+    }
+    void SetB(double valueB)
+    {
+        b = valueB;
+    }
+    void SetC(double valueC)
+    {
+        c = valueC;
+    }
+    triangle()
+        //конструктор по умолчанию
+    {
+        a = 3;
+        b = 4;
+        c = 5;
+        cout << "Конструктор по умолчанию" << endl;
+        getchar();
+    }
+    triangle(double x, double y, double z)
+        //конструктор с параметрами
+    {
+        cout << "Конструктор с параметрами" << endl;
+        a = x; b = y; c = z;
+        getchar();
+    }
+    triangle(const triangle& other)
+        // конструктор копирования
+    {
+        a = other.a;
+        b = other.b;
+        c = other.c;
+        cout << "Конструктор копирования" << endl;
+        getchar();
+    }
+    int area()
+        //функция находения площади треугольника
+    {
+        double per = (a + b + c) / 2, s = sqrt(per * (per - a) * (per - b) * (per - c));
+        //формула нахождения площади
+        return s;
+    }
+    double angleAL()
+    {
+        double alpha;
+        alpha = acos((a * a + c * c - b * b) / (2 * a * c)) * 180 / 3.14;
+        return alpha;
+    }
+    double angleBE()
+    {
+        double beta;
+        beta = acos((a * a + b * b - c * c) / (2 * a * b)) * 180 / 3.14;
+        return beta;
+    }
+    double angleGA()
+    {
+        double gamma;
+        gamma = acos((b * b + c * c - a * a) / (2 * b * c)) * 180 / 3.14;
+        return gamma;
+    }
+    int height()
+        //функция нахождения высоты
+    {
+        int n;
+        double per = (a + b + c) / 2, s = sqrt(per * (per - a) * (per - b) * (per - c)), h;
+        cout << "\nвыберите угол для подсчета высоты:\n1. Альфа\n2. Бета\n3. Гамма\n" << endl;
+        cin >> n;
+        switch (n)
+            //срабатывает определенный кейс в зависимости от того, какой угол мы выбрали
+        {
+        case 1:
+            //1-ый угол
+            h = 2 * s / a;
+            break;
+        case 2:
+            //2-ой угол
+            h = 2 * s / b;
+            break;
+        case 3:
+            //3-ий угол
+            h = 2 * s / c;
+            break;
+        default:
+            //если введено некорректное число
+            cout << "Неверное значение" << endl;
+            break;
+        }
+        return h;
+        //выводим значение высоты
+    }
+    int median()
+        //функция вывода медианы
+    {
+        double m;
+        if (a > b && a > c)
+            m = sqrt(2 * c * c + 2 * b * b - a * a) / 2;
+        if (b > a && b > c)
+            m = sqrt(2 * c * c + 2 * a * a - b * b) / 2;
+        if (c > a && c > b)
+            m = sqrt(2 * a * a + 2 * b * b - c * c) / 2;
+        else
+            m = sqrt(2 * c * c + 2 * b * b - a * a) / 2;
+        return m;
+        //вывоим значения медианы
+    }
+    friend istream& operator >> (istream& input, triangle& triangleZ)
+        //перегрузка оператора cin
+    {
+        double arr[3];
+        cout << "Перегрузка cin" << endl;
+        cout << "Сторона a" << endl;
+        cin >> arr[0];
+        cout << "Сторна b" << endl;
+        cin >> arr[1];
+        cout << "Сторона c" << endl;
+        cin >> arr[2];
+        sort(arr, arr + 3);
+        triangleZ.a = arr[0];
+        triangleZ.b = arr[1];
+        triangleZ.c = arr[2];
+        //передача аргументов
+        getchar(); getchar();
+        return cin;
+    }
+    friend ostream& operator << (ostream& output, triangle& triangleZ)
+        //перегрузка cout
+    {
+        cout << "Перегрузка cout" << endl;
+        cout << "a = " << triangleZ.a << " b = " << triangleZ.b << " c = " << triangleZ.c << endl;
+        //получение аргументов
+        getchar(); getchar();
+        return cout;
+    }
+    friend int operator^ (triangle& tA, triangle& tB)
+        //перегрузка операции ^ для обозначения операции определения подобия двух треугольников
+    {
+        return (tA.a / tB.a == tA.b / tB.b && tA.b / tB.b == tA.c / tB.c);
+    }
+
+    ~triangle()
+        //деструктор
+    {
+        cout << "Деструктор" << endl;
+        getchar();
+    }
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int menu;
+    int k;
+    double a1 = 3, b1 = 4, c1 = 5, pl, med, hei, aA, aB, aG;
+    triangle triangleA(a1, b1, c1);
+    triangle triangleB;
+    triangle triangleCpy(triangleB);
+    do
+    {
+        system("CLS");
+        cout << "1:Вычисление площади треугольника А" << endl;
+        cout << "2:Вычисление углов треугольника А" << endl;
+        cout << "3:Вычисление высоты треугольника А" << endl;
+        cout << "4:Вычисление медианы треугольника Б" << endl;
+        cout << "5:Показать стороны треугольника А" << endl;
+        cout << "6:Показать стороны треугольника Б (Get)" << endl;
+        cout << "7:Изменить стороны треугольника А" << endl;
+        cout << "8:Изменить стороны треугольника Б (Set)" << endl;
+        cout << "9:Сравнить треугольники" << endl;
+        cout << "0:Выход" << endl;
+        cin >> menu;
+        switch (menu)
+        {
+        case 1:
+        {
+            pl = triangleA.area();
+            cout << "Площадь: " << pl << endl;
+            getchar(); getchar();
+            break;
+        }
+        case 2:
+            aA = triangleA.angleAL();
+            aB = triangleA.angleBE();
+            aG = triangleA.angleGA();
+            cout << "Углы:\n   Альфа: " << aA << "\n   Бета: " << aB << "\n   Гамма: " << aG << endl;
+            getchar(); getchar();
+            break;
+        case 3:
+            hei = triangleA.height();
+            cout << "Высота: " << hei << endl;
+            getchar(); getchar();
+            break;
+        case 4:
+            med = triangleA.median();
+            cout << "Медиана: " << med << endl;
+            getchar(); getchar();
+            break;
+        case 5:
+            cout << triangleA;
+            break;
+        case 6:
+            cout << "Геттер" << endl;
+            triangleB.GetA();
+            triangleB.GetB();
+            triangleB.GetC();
+            getchar(); getchar();
+            break;
+        case 7:
+            cin >> triangleA;
+            break;
+        case 8:
+            double arrs[3];
+            cout << "Сеттер" << endl;
+            cout << "Сторона a" << endl;
+            cin >> arrs[0];
+            cout << "Сторна b" << endl;
+            cin >> arrs[1];
+            cout << "Сторона c" << endl;
+            cin >> arrs[2];
+            sort(arrs, arrs + 3);
+            triangleB.SetA(arrs[0]);
+            triangleB.SetB(arrs[1]);
+            triangleB.SetC(arrs[2]);
+            break;
+        case 9:
+        {
+            k = triangleA ^ triangleB;
+            if (k)
+                cout << "треугольники А и Б подобны" << endl;
+            else
+                cout << "треуголькик А и Б отличны" << endl;
+            getchar(); getchar();
+        }
+        }
+    } while (menu != 0);
+    return 0;
+}
---
+++ nikita/Pr2+Vis.cpp
@@ -0,0 +1,442 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <locale.h>
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
+#include <math.h>
+using namespace std;
+
+class String // класс строка
+{
+public:
+    char* s; //поле строка
+    int n; //поле длина строки
+    String()
+    {
+        //    cout << "Constructor" << endl;
+    } //конструктор поумолчанию
+    String(char* str) //конструктор с параметром си-строка
+    {
+        n = strlen(str);
+        s = new char[n + 1];
+        for (int i = 0; i < n; i++)
+            s[i] = str[i];
+        s[n] = '\0';
+        //    cout << "Constructor with parameters" << endl;
+    }
+    String(char symbol) //конструктор с параметром символ
+    {
+        s = new char[2];
+        s[0] = symbol;
+        s[1] = '\0';
+        n = 1;
+        //    cout << "Constructor with parameter - symbol" << endl;
+    }
+    String(const String& str) //конструктор копирования
+    {
+        s = str.s;
+        n = str.n;
+        //    cout << "Constructor copy" << endl;
+    }
+    String& operator= (String s1) //перегрузка оператора присвоения
+    {
+        s = s1.s;
+        n = s1.n;
+        return *this;
+        cout << "Перегразка оператора = " << endl;
+    }
+    int GetLength(String s1) //метод получения длины строки
+    {
+        cout << "Метод получения длины строки:" << endl;
+        return s1.n;
+    }
+    ~String()
+    {
+        //     cout << "Destructor" << endl;
+    } //деструктор
+};
+
+bool isId(char* str) //функция для определения является ли строка строкой-идентификатором
+{
+    const int n = 32;
+    char keywords[][n] = { "auto", "break", "case","char","const", "continue", "default", "do",
+            "double", "else", "enum", "extern", "float", "for", "goto", "if", "int", "long",
+            "register", "return", "short", "signed", "sizeof", "static", "struct", "switch",
+            "typedef", "union", "unsigned","void", "volatile", "while" };
+    if (isdigit(*str)) //проверка на наличие цифры в начале строки
+    {
+        cout << "Строка не является индентификаторм" << endl;
+        return false;
+    }
+    for (int i = 0; i < n; i++) //проверка на наличие ключевых слов
+    {
+        if (!strcmp(str, *(keywords + i)))
+        {
+            cout << "Строка не является индентификаторм" << endl;
+            return false;
+        }
+    }
+    while (isalnum(*str) || *str == '_') str++; //проверка являются ли все символы строки буквами, цифрами или нижними подчеркиваниями
+    if (!str[0])
+    {
+        cout << "Строка является индентификаторм" << endl;
+        return true;
+    }
+    else
+    {
+        cout << "Строка не является индентификаторм" << endl;
+        return false;
+    }
+}
+
+class IdString : public String //класс-наследник строка-идентификатор
+{
+public:
+    IdString()
+    {
+        //    cout << "Constructor" << endl;
+    }
+    IdString(char* str)
+    {
+        if (isId(str))
+        {
+            n = strlen(str);
+            s = new char[n + 1];
+            for (int i = 0; i < n; i++)
+                s[i] = str[i];
+            s[n] = '\0';
+        }
+        else
+        {
+            s = new char[2];
+            s[0] = '0';
+            s[1] = '\0';
+            n = 0;
+        }
+    }
+    char operator[] (int i) //перегрузка оператора индексации (возвращает указанный символ)
+    {
+        return *(s + i);
+    };
+    void FirstOcc(const IdString ids, const char c) //метод поиска первого вхождения символа в строку
+    {
+        cout << "Метод поиска первого вхождения символа в строку:" << endl;
+        int i = 0;
+        while (*(ids.s + i) != '\0')
+        {
+            if (*(ids.s + i) == c)
+            {
+                cout << "Первое вхождение символа " << c << ": элемент " << i + 1 << endl;
+                return;
+            }
+            i++;
+        }
+        cout << "Такого символа нет" << endl;
+    }
+    IdString(IdString const& str)
+    {
+        s = str.s;
+        n = str.n;
+    }
+    IdString& operator= (const IdString& s1)
+    {
+        String::operator=(s1);
+        return *this;
+    }
+    ~IdString()
+    {
+    }
+    friend IdString operator- (const IdString ids1, const IdString ids2)
+    {
+        cout << "Перегрузка операции - " << endl;
+        IdString ids3;
+        char* s3;
+        int k, i, j, n;
+        for (k = 0; *(ids2.s + k) != '\0'; k++)
+        {
+            for (i = j = 0; *(ids1.s + i) != '\0'; i++)
+                if (*(ids1.s + i) != *(ids2.s + k))
+                    *(ids1.s + j++) = *(ids1.s + i);
+            *(ids1.s + j) = '\0';
+        }
+        n = strlen(ids1.s);
+        s3 = new char[n + 1];
+        for (i = 0; i < n; i++)
+            s3[i] = ids1.s[i];
+        s3[n] = '\0';
+        ids3.s = s3;
+        ids3.n = n;
+        return ids3;
+    }
+};
+
+
+bool isBy(char* str) //дружественная функция для определения является ли строка бинарной
+{
+    while (*str == '1' || *str == '0') str++;
+    if (!str[0])
+    {
+        cout << "Строка является бинароной" << endl;
+        return true;
+    }
+    else
+    {
+        cout << "Строка не является бинароной" << endl;
+        return false;
+    }
+}
+
+class BiString : public String //класс-наследник бинарная строка
+{
+public:
+    BiString()
+    {
+    }
+    BiString(char* str)
+    {
+        bool k;
+        if (k = isBy(str))
+        {
+            n = strlen(str);
+            s = new char[n + 1];
+            for (int i = 0; i < n; i++)
+                s[i] = str[i];
+            s[n] = '\0';
+        }
+        else
+        {
+            s = new char[2];
+            s[0] = '0';
+            s[1] = '\0';
+            n = 0;
+        }
+    }
+    void WhichSign(const BiString str) //метод определения знака хранимого в строке числа
+    {
+        if (*(str.s) == '0')
+            cout << "Число положительно" << endl;
+        else
+            cout << "Число отрицательно" << endl;
+    }
+    BiString& operator= (const BiString& s1)
+    {
+        String::operator=(s1);
+        return *this;
+    }
+    BiString(BiString const& str)
+    {
+        s = str.s;
+        n = str.n;
+    }
+    ~BiString()
+    {
+    }
+    friend BiString operator+ (const BiString bs1, const BiString bs2) //перегрузка оператора сложения (не учитывает знак)
+    {
+        cout << "Перегрузка операции + без учитывания знака" << endl;
+        int i, num1, num2, sum, rem, n3;
+        char* str;
+        BiString bs3;
+        if (bs1.n > bs2.n)
+            bs3.n = bs1.n;
+        else
+            bs3.n = bs2.n;
+        n3 = bs3.n;
+        num1 = strtol(bs1.s, NULL, 2);
+        num2 = strtol(bs2.s, NULL, 2);
+        sum = num1 + num2;
+        str = new char[n3 + 2];
+        if (sum >= pow(2, n3))
+            *(str + bs3.n + 1) = '\0';
+        else
+        {
+            *(str + bs3.n) = '\0';
+            n3--;
+        }
+        for (i = n3; i >= 0; i--)
+        {
+            rem = sum % 2;
+            *(str + i) = rem + '0';
+            sum /= 2;
+        }
+        bs3.s = str;
+        return bs3;
+    }
+    friend BiString operator- (const BiString bs1, const BiString bs2) //перегрузка оператора вычитания (не учитывает знак)
+    {
+        cout << "Перегрузка операции - без учитывания знака" << endl;
+        int i, num1, num2, sub, rem, n3;
+        char* str;
+        BiString bs3;
+        if (bs1.n > bs2.n)
+            bs3.n = bs1.n;
+        else
+            bs3.n = bs2.n;
+        n3 = bs3.n;
+        num1 = strtol(bs1.s, NULL, 2);
+        num2 = strtol(bs2.s, NULL, 2);
+        sub = num1 - num2;
+        str = new char[n3 + 1];
+        if (sub <= pow(2, n3))
+            *(str + bs3.n) = '\0';
+        else
+        {
+            *(str + (bs3.n - 1)) = '\0';
+            n3--;
+        }
+        for (i = n3 - 1; i >= 0; i--)
+        {
+            rem = sub % 2;
+            *(str + i) = rem + '0';
+            sub /= 2;
+        }
+        *(str + bs3.n) = '\0';
+        bs3.s = str;
+        return bs3;
+    }
+};
+
+char* GetString(char* s) //функция ввода строки с клавиатуры
+{
+    fgets(s, 100, stdin);
+    s[strcspn(s, "\n")] = 0;
+    fflush(stdin);
+    return s;
+}
+
+int main()
+
+{
+    int n = 0, menu, men, me; char* s; char c;
+    setlocale(LC_ALL, "Russian");
+    s = new char[100];
+    do 
+    {
+        system("CLS");
+        cout << "1:Строка" << endl;
+        cout << "2:Строка-индетификатор" << endl;
+        cout << "3:Бинарная строка" << endl;
+        cout << "0.Выход" << endl;
+        cin >> menu;
+        cin.get();
+        switch (menu)
+        {
+        case 1:
+        {
+            bool flag;
+            puts("Введите строку: ");
+            s = GetString(s);
+            String str(s); //создание объекта класса строка
+            puts("Провести определение длины строки?");
+            puts("1/0");
+            cin >> flag;
+            if (flag)
+            {
+                n = str.GetLength(str); //проверка метода определения длины строки
+                cout << n << endl;
+                cin.get(); getchar();
+            }
+            break;
+        }
+        case 2:
+        {
+            do
+            {
+                system("CLS");
+                cout << "1:Проверка метода поиска первого вхождения символа в строку" << endl;
+                cout << "2:Проверка перегруженного оператора вычитания" << endl;
+                cout << "0:Пропуск" << endl;
+                cin >> men;
+                cin.get();
+                switch (men)
+                {
+                case 1:
+                {
+                    puts("Введите строку-индетификатор: ");
+                    s = GetString(s);
+                    IdString id1(s); //создание объекта класса строка-идентификатор
+                    puts("Введите символ: ");
+                    cin >> c;
+                    fflush(stdin);
+                    id1.FirstOcc(id1, c); //проверка метода поиска первого вхождения символа в строку
+                    cin.get(); getchar();
+                    break;
+                }
+                case 2:
+                    puts("Введите 1-ую строку-индетификатор: ");
+                    s = GetString(s);
+                    IdString id1(s); //создание объекта класса строка-идентификатор                
+                    puts("Введите 2-ую строку-индетификатор: ");
+                    s = GetString(s);
+                    IdString id2(s); //создание второго объекта класса строка-идентификатор
+                    puts("1-ая строка индетификатор - 2-ая строка идентификатор = ");
+                    IdString id3;
+                    id3 = id1 - id2; //проверка перегруженного оператора вычитания
+                    puts(id3.s);
+                    cin.get(); getchar();
+                    break;
+                }
+
+            } while (men != 0);
+            break;
+        }
+        case 3:
+        {          
+            do
+            {
+                system("CLS");
+                cout << "1: Определение знака у двоичного числа" << endl;
+                cout << "2: Проверка перегруженного оператора вычитания" << endl;
+                cout << "3: Проверка перегруженного оператора сложения" << endl;
+                cout << "0: Пропуск" << endl;
+                cin >> me;
+                cin.get();
+                switch (me)
+                {
+                case 1:
+                {
+                    puts("Введите бинарную строку ");
+                    s = GetString(s);
+                    BiString bi1(s); //создание объекта класса бинарная строка
+                    bi1.WhichSign(bi1); //проверка метода определения знака у двоичного числа
+                    cin.get(); getchar();
+                    break;
+                }
+                case 2:
+                {
+                    puts("Введите 1-ую бинарную строку ");
+                    s = GetString(s);
+                    BiString bi1(s); //создание объекта класса бинарная строка
+                    puts("Введите 2-ую бинарную строку: ");
+                    s = GetString(s);
+                    BiString bi2(s); //создание второго объекта класса бинарная строка
+                    puts("1-ая бинарная строка - 2-ая бинарная строка = ");
+                    BiString bi3;
+                    bi3 = bi1 - bi2; //проверка перегруженного оператора вычитания
+                    puts(bi3.s);
+                    cin.get(); getchar();
+                    break;
+                }
+                case 3:
+                    puts("Введите 1-ую бинарную строку ");
+                    s = GetString(s);
+                    BiString bi1(s); //создание объекта класса бинарная строка
+                    puts("Введите 2-ую бинарную строку: ");
+                    s = GetString(s);
+                    BiString bi2(s); //создание второго объекта класса бинарная строка
+                    puts("1-ая бинарная строка + 2-ая бинарная строка = ");
+                    BiString bi3;
+                    bi3 = bi1 + bi2; //проверка перегруженного оператора сложения
+                    puts(bi3.s);
+                    cin.get(); getchar();
+                    break;
+                }
+            } while (me != 0);
+            break;
+
+        }
+        }
+    } while (menu != 0);
+    return 0;
+}
